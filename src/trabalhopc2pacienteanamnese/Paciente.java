48f7c36814a30c83d5323a06e7379934b3241e8d
Augusto <guto58ra@gmail.com>
2022-09-17 20:11:30 -0300

    .

---
+++ src/trabalhopc2pacienteanamnese/Paciente.java
@@ -0,0 +1,9 @@
+package trabalhopc2pacienteanamnese;
+
+public class Paciente {
+    
+    long numCNS;
+    String nome, nomeMae;
+    Enum sexo;
+    Endereco endereco;
+}
---
+++ src/trabalhopc2pacienteanamnese/TrabalhoPC2PacienteAnamnese.java
@@ -0,0 +1,16 @@
+package trabalhopc2pacienteanamnese;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TrabalhoPC2PacienteAnamnese {
+    
+    static Scanner sc = new Scanner(System.in);
+    static Random rd = new Random();
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
